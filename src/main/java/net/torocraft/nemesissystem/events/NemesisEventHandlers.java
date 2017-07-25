992ab73bd82d3b2db0779284a70f0e6e4b33eb23
Charles Howard <frodare@gmail.com>
2017-07-24 22:29:17 -0400

    working on events and notifications

--- src/main/java/net/torocraft/nemesissystem/events/NemesisEventHandlers.java
+++ src/main/java/net/torocraft/nemesissystem/events/NemesisEventHandlers.java
@@ -1,70 +1,67 @@
 package net.torocraft.nemesissystem.events;
 
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.torocraft.nemesissystem.NemesisSystem;
 import net.torocraft.nemesissystem.registry.LogEntry;
 
 public class NemesisEventHandlers {
 
-	@SubscribeEvent
-	public void promotion(NemesisEvent.Promotion event) {
-		ITextComponent message = buildMessage("notifications.nemesis_system.promotion",
-				event.getNemesis().getNameAndTitle(), event.getNemesis().getLevel());
-		sendGlobalMessage(message);
+	private static final String PREFIX = "notifications";
 
-		event.getNemesis().addToHistory(LogEntry.PROMOTION(event.getNemesis().getLevel()));
+	@SubscribeEvent
+	public void promotion(NemesisEvent.Promotion ev) {
+		send("promotion", title(ev), ev.getNemesis().getLevel());
+		ev.getNemesis().addToHistory(LogEntry.PROMOTION(ev.getNemesis().getLevel()));
 	}
 
 	@SubscribeEvent
-	public void demotion(NemesisEvent.Demotion event) {
-		ITextComponent message = buildMessage("notifications.nemesis_system.demotion",
-				event.getNemesis().getNameAndTitle(), event.getNemesis().getLevel(), event.getSlayerName());
-		sendGlobalMessage(message);
-
-		event.getNemesis().addToHistory(LogEntry.PROMOTION(event.getNemesis().getLevel()));
+	public void demotion(NemesisEvent.Demotion ev) {
+		send("demotion", title(ev), ev.getNemesis().getLevel(), ev.getSlayerName());
+		ev.getNemesis().addToHistory(LogEntry.PROMOTION(ev.getNemesis().getLevel()));
 	}
 
 	@SubscribeEvent
-	public void duel(NemesisEvent.Duel event) {
-		ITextComponent message = buildMessage("notifications.nemesis_system.duelIfCrowded",
-				event.getWinner().getNameAndTitle(), event.getLoser().getNameAndTitle());
-		sendGlobalMessage(message);
-
-		event.getWinner().addToHistory(LogEntry.DUEL_WIN(event.getLoser().getNameAndTitle()));
-		event.getLoser().addToHistory(LogEntry.DUEL_LOSS(event.getWinner().getNameAndTitle()));
+	public void duel(NemesisEvent.Duel ev) {
+		send("duelIfCrowded", title(ev), ev.getLoser().getNameAndTitle());
+		ev.getWinner().addToHistory(LogEntry.DUEL_WIN(ev.getLoser().getNameAndTitle()));
+		ev.getLoser().addToHistory(LogEntry.DUEL_LOSS(ev.getWinner().getNameAndTitle()));
 	}
 
 	@SubscribeEvent
-	public void register(NemesisEvent.Register event) {
-		ITextComponent message = buildMessage("notifications.nemesis_system.register",
-				event.getNemesis().getNameAndTitle(), event.getNemesis().getX(), event.getNemesis().getZ());
-		sendGlobalMessage(message);
-
-		event.getNemesis().addToHistory(LogEntry.CREATION(event.getNemesis().getX(), event.getNemesis().getZ()));
+	public void register(NemesisEvent.Register ev) {
+		send("register", title(ev), ev.getNemesis().getX(), ev.getNemesis().getZ());
+		ev.getNemesis().addToHistory(LogEntry.CREATION(ev.getNemesis().getX(), ev.getNemesis().getZ()));
 	}
 
 	@SubscribeEvent
 	public void death(NemesisEvent.Death event) {
-		ITextComponent message = buildMessage("notifications.nemesis_system.death",
-				event.getNemesis().getNameAndTitle(), event.getSlayerName());
-		sendGlobalMessage(message);
-
+		send("death", title(event), event.getSlayerName());
 		event.getNemesis().addToHistory(LogEntry.DIED(event.getSlayerName()));
 	}
 
-	// TODO need an event for killing a player
+	private String title(NemesisEvent event) {
+		return event.getNemesis().getNameAndTitle();
+	}
+
+	private void send(String translationKey, Object... args) {
+		TextComponentTranslation m = new TextComponentTranslation(PREFIX  + "." + translationKey, args);
+		m.setStyle(new Style());
+		m.getStyle().setColor(TextFormatting.RED);
+		sendGlobalMessage(m);
+	}
 
 	private void sendGlobalMessage(ITextComponent message) {
+		if (NemesisSystem.SERVER == null) {
+			return;
+		}
 		try {
 			NemesisSystem.SERVER.getPlayerList().getPlayers().forEach(player -> player.sendMessage(message));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	private ITextComponent buildMessage(String translationKey, Object... args) {
-		return new TextComponentTranslation(translationKey, args);
 	}
 }
