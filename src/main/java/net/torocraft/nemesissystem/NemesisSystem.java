d327daa8fb502810a302bc63366bf4b8aec7c99d
Charles Howard <frodare@gmail.com>
2017-09-23 21:37:59 -0400

    version step to 2

--- src/main/java/net/torocraft/nemesissystem/NemesisSystem.java
+++ src/main/java/net/torocraft/nemesissystem/NemesisSystem.java
@@ -22,7 +22,7 @@ import net.torocraft.nemesissystem.registry.NemesisEntry;
 public class NemesisSystem {
 
 	public static final String MODID = "nemesissystem";
-	public static final String VERSION = "1.12.1-1";
+	public static final String VERSION = "1.12.1-2";
 	public static final String MODNAME = "NemesisSystem";
 
 	public static final String NBT_NEMESIS_ID = "torocraft_nemesis_id";
